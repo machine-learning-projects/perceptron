9b8d4b133a6ca7272219dabe9d11117461f84584
SZanlongo <SZanlongo@users.noreply.github.com>
2015-02-18 11:36:03 -0500

    Perceptron now finds error rate over size-100 intervals of test data

--- perceptron.m
+++ perceptron.m
@@ -1,17 +1,24 @@
-function [w, b] = perceptron(X, Y)
+% function [w, b] = perceptron(X, Y, F, L)
+function [err] = perceptron(X, Y, F, L)
 
 w = zeros(1, size(X,2)); b = 0; k = 0; eta = 1;
 
-
 % R = max(sum(X.^2, 2));    % R^2 value
-
 R = 1;  %R^2 = 1, so this is hard-coded to save time
 
-u = 1;  % interval counter
+err = [];
 
-while u < 19200 % less than number of features
+u = 1;  % interval counter
+u_e = 1;
+
+while u_e < size(X, 1) % less than number of features
 %     for i = 1 : size(X, 1)	% go over articles
-    for i = u : u + 99
+    u_e = u + 99;
+    if (u_e > size(X, 1))
+        u_e = size(X, 1);
+    end
+    
+    for i = u : u_e
         if (Y(i) * (dot(w, X(i, :)) + b)) <= 0    % wrong?
             w = w + eta * Y(i) * X(i, :);   % add point to weight vector
             b = b + eta * Y(i) * R;   % update bias
@@ -21,8 +28,20 @@ while u < 19200 % less than number of features
     
     u = u + 100; % increment for next set of 100
     
-    k   % total number of errors
+%     k   % total number of mistakes
+    
+    incorrect = 0;  % number of incorrectly classified tests
+    
+    for i = 1 : size(F, 1)  % go over the test articles
+        if sign(dot(w, F(i, :)) + b) ~= L(i)    % incorrectly classified
+            incorrect = incorrect + 1;
+        end
+    end
+    
+    err(end + 1) = incorrect / u * 100;
     
 end
+
+err
 
 end
--- test.m
+++ test.m
@@ -6,12 +6,14 @@ Y = load('rcv1.train.labels');
 
 % test features
 F = spconvert(load('rcv1.test.features'));
+F = padarray(F, [0 (size(X, 2) - size(F, 2))], 0, 'post');
 % test labels
 L = load('rcv1.test.labels');
 
 % training orders
 O = load('training.orders');
 
-[w, b] = perceptron(X, Y);
+% [w, b] = perceptron(X, Y, F, L);
+perceptron(X, Y, F, L);
 
-b
+% b
